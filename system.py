4c90223a27f4e9bd76b49d2d5ca219292b996717
Aviv Ben-Yosef <aviv@crowdspot.com>
2010-11-10 22:11:56 +0200

    adding dir initializer

--- system.py
+++ system.py
@@ -1,3 +1,8 @@
 class Directory(object):
 	def initialize(self, path):
 		self.path = path
+
+class DirectoryInitializer(object):
+	def initialize_directory(self, directory):
+		# Here some system logic happens to get a valid path
+		directory.initialize('initialized_path')
--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import unittest
-from system import Directory
+from system import Directory, DirectoryInitializer
 
 class DirectoryTest(unittest.TestCase):
 	def test_can_be_initialized(self):
 		d = Directory()
 		d.initialize('path')
 		assert d.path == 'path'
+
+class DirectoryInitializerTest(unittest.TestCase):
+	def test_initializes_directory(self):
+		d = Directory()
+		initializer = DirectoryInitializer()
+		initializer.initialize_directory(d)
+		assert d.path == 'initialized_path'
+		
